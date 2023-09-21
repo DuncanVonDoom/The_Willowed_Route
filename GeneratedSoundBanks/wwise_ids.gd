c53fc916dfba32bebd237ba87e10a1e64c42fbcb
Duncan <thedunknvondoom@gmail.com>
2023-09-21 00:06:45 -0400

    I did a wise thing!

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,60 @@
+class_name AK
+
+class EVENTS:
+
+	const _dict = {}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const ENGINESPEED = 3350583156
+
+	const _dict = {
+		"EngineSpeed": ENGINESPEED
+	}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+
+	const _dict = {
+		"Init": INIT
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
+
+	const _dict = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
